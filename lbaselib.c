89ae72dd3a69bea731a21b30857e0267dfbf9a3a
TocnoWamGovoriyYaNeHacer201145 <hacergames861@gmail.com>
2025-11-14 23:01:48 +0300

    Update lbaselib.c

--- lbaselib.c
+++ lbaselib.c
@@ -579,7 +579,7 @@ static const luaL_Reg base_funcs[] = {
   {"next", luaB_next},
   {"pairs", luaB_pairs},
   {"pcall", luaB_pcall},
-  {"print", luaB_print},
+  {"show", luaB_print},
   {"warn", luaB_warn},
   {"rawequal", luaB_rawequal},
   {"rawlen", luaB_rawlen},
