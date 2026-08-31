17c2d7cf31fa1854db0e5d65a90e4d06f163acd5
TocnoWamGovoriyYaNeHacer201145 <hacergames861@gmail.com>
2026-08-31 19:30:17 +0300

    Renamed variable keywords
    renamed public to pub
    renamed private to loc

--- llex.c
+++ llex.c
@@ -44,8 +44,8 @@
 /* ORDER RESERVED */
 static const char *const luaX_tokens [] = {
     "and", "break", "do", "else", "elseif",
-    "end", "false", "for", "func", "public", "goto", "if",
-    "in", "private", "null", "not", "or", "repeat",
+    "end", "false", "for", "func", "pub", "goto", "if",
+    "in", "loc", "null", "not", "or", "repeat",
     "return", "then", "true", "until", "while",
     "//", "..", "...", "==", ">=", "<=", "~=",
     "<<", ">>", "::", "<eof>",
